481ed466c028afe04b7e0dbf7f8558a143f6dc44
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-02-06 16:57:13 +0700

    Release 0

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function Dictionary(arr) {
+    // your code here
+    for (var i = 0; i < arr.length; i++) {
+        let reset = arr[i]
+        for (var j = i-1; j >= 0 && arr[j] > reset ; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = reset
+    }
+      return arr.join(',')
+}
+
+let arrOfWord = ['makan', 'duduk', 'tidur', 'terbang']
+let arrOfName = ['anggi', 'angga', 'ani', 'adi']
+
+console.log(Dictionary(arrOfWord)); //duduk,makan,terbang,tidur
+console.log(Dictionary(arrOfName)); //angga,anggi,adi,ani
 
 module.exports = Dictionary
