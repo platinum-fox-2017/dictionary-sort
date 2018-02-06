22785435179ceba0145628e0b8facb5271ace9e5
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-06 18:42:46 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
 
-module.exports = Dictionary
+function dic(arrOfWord){
+  for(var i=1; i<arrOfWord.length; i++){
+    for(var j=0; j<=i-1; j++){
+      var belakang = arrOfWord[i];
+      var depan = arrOfWord[j];
+      if(arrOfWord[i]<arrOfWord[j]){
+        arrOfWord[j]=belakang;
+        arrOfWord[i]=depan;
+      }
+    }
+  }
+  return arrOfWord;
+}
+
+console.log(dic(['makan','duduk','tidur','terbang'])); //duduk, makan, terbang, tidur
+console.log(dic(['anggi','angga','ani','adi','aid'])); //adi, angga, anggi, ani
+
+// module.exports = Dictionary
