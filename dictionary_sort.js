d8bec44fbc6e0ef6626241bc600ac5a06f02b8af
yosaid92 <yohanessahrul92@gmail.com>
2018-02-06 22:20:08 +0700

    add dictionary-sort-challenge

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,35 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+    function dictionarySort(arr){
+        if(arr.length <= 1){
+            return arr;
+        } else {
+            //cari nilai minimum
+            var sortWord = [];
+            var minWord = arr[0];
+            for(var i=0; i<arr.length; i++){
+                if(arr[i] < minWord){
+                    minWord = arr[i];
+                }
+            }
+
+            for(var j=0; j<arr.length; j++){
+                if(arr[j] == minWord){
+                    sortWord.push(arr[j]);
+                    arr.splice(j,1);
+                }
+            }
+
+            return sortWord.concat(dictionarySort(arr));
+
+        }
+
+    }
+    
+    console.log(dictionarySort(['doel','magna','avatar']));
+    console.log(dictionarySort(['anggi','angga','ada','ani','adi']));
+    console.log(dictionarySort(['makan','duduk','tidur','terbang']));
+
+// module.exports = {
+//     Dictionary
+// };
