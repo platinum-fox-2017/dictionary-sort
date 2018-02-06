925229b1d5951c7b03ae23ff9f896f9fad8afc85
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-06 16:49:24 +0700

    first Commit

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,30 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+var arrOfWord1=['makan','duduk','tidur','terbang'];
+var arrOfWord2=['anggi','angga','ani','adi'];
+
+function wordSort(arr) {
+    for(var i=1; i<arr.length; i++) {
+        for(var j=0; j<i; j++) {
+          if(arr[i]<arr[j]) {
+            var temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+          }
+        }
+      }
+
+    return arr.join(',');
+}
+
+function min(arr) {
+    var min=arr[0]
+    for(var i in arr) {
+        if(min>arr[i]) {min=arr[i]}
+    }
+    console.log(min);
+}
+console.log(wordSort(arrOfWord1));
+console.log(wordSort(arrOfWord2));
+
