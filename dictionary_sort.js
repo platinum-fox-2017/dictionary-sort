864003f23b886c9ad9084b99a2e9e6e03b849f5a
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-06 15:23:04 +0700

    release 0 done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+let Dictionary = (input) => {
+    for(let i=0; i<input.length; i++){
+        let min =input[i];
+        if(input[i]>input[i+1]){
+            min = input[i+1];
+        }
+        if(min !== input[i]){
+            let temp = input[i];
+            input[i] = input[i+1];
+            input[i+1] = temp;
+        }
+    }
+    return input;
+}
+
+let inputA = ['makan','duduk','tidur','terbang'];
+let inputB = ['anggi','angga','ani','adi'];
+
+console.log(Dictionary(inputA));
+console.log(Dictionary(inputB));
+
 
 module.exports = Dictionary
