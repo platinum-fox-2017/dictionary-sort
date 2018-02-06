8a9cd981ede1fa3b5c3806f4f2b973332301d1f8
agrha <agrhakartz@gmail.com>
2018-02-06 18:38:57 +0700

    release 0 dict sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,17 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function Dictionary(arr){
+  let temp = 0
+  for(i=0;i<arr.length-1;i++){
+    if(arr[i+1]<arr[i]){
+      temp = arr[i]
+      arr[i] = arr[i+1]
+      arr[i+1] = temp
+      i = -1
+      console.log(arr);
+    }
+  }
+  return arr
+}
+
+console.log(Dictionary(['makut','servi','juhi','logres']));
