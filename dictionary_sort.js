6249f90840161a133ea68c1a39a7807a63e0a6cf
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-02-06 17:23:20 +0700

    done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,24 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
 
-module.exports = Dictionary
+function sortDictionary(arrWords) {
+    // return arrWords.sort()
+    for (let i = 0; i < arrWords.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arrWords.length; j++) {
+            if (arrWords[j].toLowerCase() < arrWords[min].toLowerCase()) {
+                min = j
+            }
+        }
+        let temp = arrWords[i]
+        arrWords[i] = arrWords[min]
+        arrWords[min] = temp
+    }
+    return arrWords
+}
+
+
+// let arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+let arrOfWord = ['anggi', 'Angga', 'Ani', 'adi'];
+console.log(sortDictionary(arrOfWord));
+
+// module.exports = Dictionary
