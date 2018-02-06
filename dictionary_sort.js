cfd4848f29aa93d60c7a906d302f2a303cd2ac12
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-06 14:40:50 +0700

    add dictionary_sort.js

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,40 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+function sameFirstAlphabet(word1, word2) {
+    for(var i = 1; i < word1.length; i++) {
+        if (word1[i] < word2[i]) {
+            return word1;
+        } else if (word1[i] > word2[i]) {
+            return word2;
+        }
+    }
+    return word1;
+}
 
-module.exports = Dictionary
+function dictionarySort(arr) {
+    for(var i = 1; i < arr.length; i++) {
+        for(var j = 0; j < i; j++) {
+            if (arr[i][0] < arr[j][0]) {
+                var temp = arr[j];
+                arr[j]   = arr[i];
+                arr[i]   = temp;
+            } else if (arr[i][0] == arr[j][0]) {
+                var winner = sameFirstAlphabet(arr[i][0],arr[j][0]);
+                if (winner == arr[i][0]) {
+                    var temp = arr[j];
+                    arr[j]   = arr[i];
+                    arr[i]   = temp;
+                } else {
+                    var temp = arr[i];
+                    arr[i]   = arr[j];
+                    arr[j]   = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+var arrOfWord2 = ['anggi', 'angga', 'ani', 'adi'];
+
+console.log(dictionarySort(arrOfWord));
+console.log(dictionarySort(arrOfWord2));
