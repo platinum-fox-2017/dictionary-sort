c4cc270aeb9e6b41659baf8b8b3537b68f060727
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-06 15:58:56 +0700

    Release 0: Sort and Print

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,19 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
 
-module.exports = Dictionary
+function Dictionary(arr) {
+	for (var i = 1; i < arr.length; i++) {
+	  	for (var j = 0; j < i; j++) {
+	  		if (arr[i] < arr[j]) {
+	  			var temp = arr[i];
+	  			arr[i] = arr[j];
+	  			arr[j] = temp;
+	  		}
+	  	}
+	}
+  return arr
+}
+
+console.log(Dictionary(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(Dictionary(['angga', 'anggi', 'ani', 'adi']));
+
+// module.exports = Dictionary
