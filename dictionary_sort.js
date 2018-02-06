12e28fd8decd25f51a029904605cc43e788fdf2a
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 15:03:34 +0700

    updated dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,36 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+
+var nama = ['kevin','ketek','kanu','kenny']
+
+
+
+function ownSort(arr) {
+  
+  let nameOlah = arr
+
+  for(let i = 0;i < nameOlah.length; i++){
+    let nilaiMinimum = nameOlah[i]
+    let nilaiMinimumIndex = i
+    let count = 0
+    for(let j = i+1;j < nameOlah.length;j++){
+      
+       if(nameOlah[j] < nilaiMinimum){
+        nilaiMinimum = nameOlah[j]
+        nilaiMinimumIndex = j
+        count++
+       } 
+    }
+
+    if(count > 0){
+      let tmp = nameOlah[i]
+      nameOlah[i] = nameOlah[nilaiMinimumIndex]
+      nameOlah[nilaiMinimumIndex] = tmp
+    }
+  }
+
+  return nameOlah
+}
+
+console.log(ownSort(nama))
 
 module.exports = Dictionary
