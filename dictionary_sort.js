d75c949604a9cab06aa37adea15cfb07f69bca8e
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 11:59:07 +0700

    dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -2,24 +2,21 @@ function dictionarySort(input) {
   var arr=[];
   var temp=0;
 
-  for(var j=0;j<input.length;j++)
-    {
+  for(var j=0;j<input.length;j++){
         temp=j;
-        for(var i=j+1;i<input.length;i++)
-          {
-              if(input[i].toLowerCase()<input[temp].toLowerCase())
-                {
+        for(var i=j+1;i<input.length;i++) {
+              if(input[i].toLowerCase()<input[temp].toLowerCase()){
                   temp=i;
-                }
-          }
+              }
+        }
 
-          arr=input[j];
-          input[j]=input[temp];
-          input[temp]=arr;
+        arr=input[j];
+        input[j]=input[temp];
+        input[temp]=arr;
     }
+    
     return input.join();
 }
-
 
 console.log(dictionarySort(['makan','duduk','tidur','terbang']));
 console.log(dictionarySort(['anggi','angga','ani','adi']));
