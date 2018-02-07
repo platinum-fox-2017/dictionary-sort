3e157e8e69a5701e61badb45ea839747991b33c9
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-07 11:09:22 +0700

    Release 0: rev1

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,9 +1,9 @@
 // const readline = require('readline');
 
-function Dictionary(arr) {
+function dictionarySort(arr) {
 	for (var i = 1; i < arr.length; i++) {
 	  	for (var j = 0; j < i; j++) {
-	  		if (arr[i] < arr[j]) {
+	  		if (arr[i].toLowerCase() < arr[j].toLowerCase()) {
 	  			var temp = arr[i];
 	  			arr[i] = arr[j];
 	  			arr[j] = temp;
@@ -13,7 +13,8 @@ function Dictionary(arr) {
   return arr
 }
 
-console.log(Dictionary(['makan', 'duduk', 'tidur', 'terbang']));
-console.log(Dictionary(['angga', 'anggi', 'ani', 'adi']));
+// console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+// console.log(dictionarySort(['angga', 'anggi', 'ani', 'adi']));
+console.log(dictionarySort(['makan','DuDuK','tIdUr','Terbang']));
 
 // module.exports = Dictionary
