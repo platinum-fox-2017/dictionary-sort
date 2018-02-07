fd40c79efe9cdf6c5e4962efa1530643fde11c3a
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 11:18:00 +0700

    REVISI DICTIONARY

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -2,27 +2,25 @@ function dictionarySort(input) {
   var arr=[];
   var temp=0;
 
-
-for(var j=0;j<input.length;j++)
-  {
-    temp=j;
-    for(var i=j+1;i<input.length;i++)
-        {
-          if(input[i]<input[temp])
-              {
-                temp=i;
-              }
-
+  for(var j=0;j<input.length;j++)
+    {
+        temp=j;
+        for(var i=j+1;i<input.length;i++)
+          {
+              if(input[i].toLowerCase()<input[temp].toLowerCase())
+                {
+                  temp=i;
+                }
           }
 
-    arr=input[j];
-    input[j]=input[temp];
-    input[temp]=arr;
-
-  }
-      return input.join();
+          arr=input[j];
+          input[j]=input[temp];
+          input[temp]=arr;
+    }
+    return input.join();
 }
 
 
 console.log(dictionarySort(['makan','duduk','tidur','terbang']));
 console.log(dictionarySort(['anggi','angga','ani','adi']));
+console.log(dictionarySort(['makan','DuDuK','tIdUr','Terbang']));
