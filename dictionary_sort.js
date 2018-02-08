6e4c889936c695b249939f99889fa154c10297ae
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-08 12:05:09 +0700

    mas cara baru nich

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,24 +1,32 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 function Dictionary(arrName) {
-  let newArr = []
   for (var i = 0; i < arrName.length; i++) {
-    newArr.push(arrName[i].toLowerCase())
-
-  }
-  for (var i = 0; i < newArr.length; i++) {
-    let value = newArr[i]
-    let value2 = arrName[i]
-    console.log(value);
-    for (var j = i - 1; j >= 0 && newArr[j] > value; j--) {
-      newArr[j + 1] = newArr[j]
-      arrName[j+1] = arrName[j]
+    let value = arrName[i]
+    for (var j = i - 1; j >= 0 && arrName[j].toLowerCase() > arrName[i].toLowerCase(); j--) {
+      arrName[j + 1] = arrName[j]
     }
-    newArr[j + 1] = value
-    arrName[j + 1] = value2
+    arrName[j + 1] = value
   }
-
   return arrName
+  // let newArr = []
+  // for (var i = 0; i < arrName.length; i++) {
+  //   newArr.push(arrName[i].toLowerCase())
+  //
+  // }
+  // for (var i = 0; i < newArr.length; i++) {
+  //   let value = newArr[i]
+  //   let value2 = arrName[i]
+  //   console.log(value);
+  //   for (var j = i - 1; j >= 0 && newArr[j] > value; j--) {
+  //     newArr[j + 1] = newArr[j]
+  //     arrName[j+1] = arrName[j]
+  //   }
+  //   newArr[j + 1] = value
+  //   arrName[j + 1] = value2
+  // }
+  //
+  // return arrName
 }
 
 console.log(Dictionary(['makan','DuDuK','tIdUr','Terbang']));
