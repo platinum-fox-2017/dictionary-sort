d7b936aa4a90026ea9de6d69d01052c4e2318e7c
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-08 11:55:20 +0700

    udah bener mas

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,16 +1,26 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 function Dictionary(arrName) {
+  let newArr = []
   for (var i = 0; i < arrName.length; i++) {
-    let value = arrName[i]
-    for (var j = i - 1; j >= 0 && arrName[j] > value; j--) {
-      arrName[j + 1] = arrName[j]
-    }
-    arrName[j + 1] = value
+    newArr.push(arrName[i].toLowerCase())
+
   }
+  for (var i = 0; i < newArr.length; i++) {
+    let value = newArr[i]
+    let value2 = arrName[i]
+    console.log(value);
+    for (var j = i - 1; j >= 0 && newArr[j] > value; j--) {
+      newArr[j + 1] = newArr[j]
+      arrName[j+1] = arrName[j]
+    }
+    newArr[j + 1] = value
+    arrName[j + 1] = value2
+  }
+
   return arrName
 }
 
-console.log(Dictionary(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(Dictionary(['makan','DuDuK','tIdUr','Terbang']));
 console.log(Dictionary(['anggi', 'angga', 'ani', 'adi']));
 module.exports = Dictionary
