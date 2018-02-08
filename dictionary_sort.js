66b15e600475e4a0eaf26d80dbab9bbb6911f079
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-08 21:53:35 +0700

    Update upper & lower case bug

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 let Dictionary = (input) => {
     for(let i=0; i<input.length; i++){
         let min =input[i];
-        if(input[i]>input[i+1]){
+        if(input[i+1] !== undefined && input[i].toLowerCase()>input[i+1].toLowerCase()){
             min = input[i+1];
         }
         if(min !== input[i]){
@@ -18,9 +18,15 @@ let Dictionary = (input) => {
 
 let inputA = ['makan','duduk','tidur','terbang'];
 let inputB = ['anggi','angga','ani','adi'];
+let inputC = ['makan','DuDuK','tIdUr','Terbang'];
 
 console.log(Dictionary(inputA));
 console.log(Dictionary(inputB));
+console.log(Dictionary(inputC));
+// kalo input console.log(dictionarySort(['makan','DuDuK','tIdUr','Terbang']));
+// hasilnya salah : DuDuK,Terbang,makan,tIdUr
+// harusnya : DuDuK,Terbang,tIdUr,makan
+// RALAT ya, harusnya hasil nya : DuDuK, makan, Terbang,tIdUr
 
 
 module.exports = Dictionary
