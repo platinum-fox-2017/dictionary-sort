16cb4a35de23a0c20acb0e1f9ec916e5a4b3d4a3
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-08 13:08:07 +0700

    Menambahkan fitur untuk case Uppercase

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -5,7 +5,7 @@ const dictionary_sort = (input) => {
     for (let i = 0; i < input.length; i++) {
         let min = input[i]
         for (let j = i + 1; j < input.length; j++) {
-            if (input[j] < min) {
+            if (input[j].toLowerCase() < min.toLowerCase()) {
                 min = input[j]
                 input[j] = input[i]
                 input[i] = min
@@ -16,8 +16,10 @@ const dictionary_sort = (input) => {
     return input
 }
 
-let input1 = ['makan', 'duduk', 'tidur', 'terbang']
+let input1 = ['mAkan', 'DuDuk', 'tidur', 'teRbang']
 let input2 = ['anggi', 'angga', 'ani', 'adi']
 // module.exports = Dictionary
 console.log(dictionary_sort(input1))
+// duduk makan terbang tidur
 console.log(dictionary_sort(input2))
+// adi angga anggi ani
